f6b104d5450a9dbc5454259e191c9fa81f8795b5
sunita <sunitakim123@gmail.com>
2020-11-19 15:42:16 +0530

    test

---
+++ src/test/java/com/two.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class two {
+	WebDriver driver;
+	@Test(priority=1)
+	public void BrowserLauch()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		//driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.get("https://awspf.com/");
+		
+	}
+	
+	@Test(priority=2)
+	public void login()
+	{ 
+		driver.findElement(By.cssSelector("span.loginButton")).click();
+		driver.findElement(By.id("email")).sendKeys("sunitakim123@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("thankyou");
+		driver.findElement(By.cssSelector("input.accountActionButton")).click();
+	}
+}
